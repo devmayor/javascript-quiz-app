be186c469123378a7be5e4df6a859b1958da0f0a
Afolabi Mayowa <afotrick2011@gmail.com>
2019-09-12 12:00:39 +0100

    ready to fork and create replica with graphql

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ const MonitorRoutes = require('./routes/monitor');
 // register events
 const {Event} = require('./app/classes/events');
 
-
+// import graphql
+const graphqlHttp = require('express-graphql');
+const graphqlSchema = require('./graphql/schema');
+const graphqlResolvers = require('./graphql/resolvers');
 let app = express();
 
 app.use(bodyParser.json());
@@ -20,6 +23,12 @@ app.use('/todos', TodoRoutes);
 
 app.use('/monitor' , MonitorRoutes);
 
+app.use('/graphql',graphqlHttp({
+    schema: graphqlSchema,
+    rootValue: graphqlResolvers,
+    graphiql: true
+}))
+// create an api for managing errors for api calls thats are never found
 
 app.listen(3000,(resp)=>{
     console.log("server running")
